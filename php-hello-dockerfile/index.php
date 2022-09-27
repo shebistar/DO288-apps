c62b38ed5c4a6322dcab4c9c71f8525165925a4c
shebistar <46504606+shebistar@users.noreply.github.com>
2022-09-27 14:13:13 +0200

    Actualizar fecha y hora de la versión

--- php-hello-dockerfile/index.php
+++ php-hello-dockerfile/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hola, Mundo! Mi versión de PHP es: " . PHP_VERSION . "\n";
+print "Buenos días, la última modificación fue realizada a las 14:11 CET el 27 de Septiembre. Hola, Mundo! Mi versión de PHP es: " . PHP_VERSION . "\n";
 ?>
